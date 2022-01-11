01feb16c1f44c67144a9f608702a8c33e4a36a51
BaoBao263 <97078202+BaoBao263@users.noreply.github.com>
2022-01-11 10:26:54 +0700

    commit
    bai tap collection view

---
+++ BTUIKIT/ViewController/ScreenCollectionView/AlbumViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AlbumViewController.swift
+//  BTUIKIT
+//
+//  Created by Mac on 1/11/22.
+//  Copyright © 2022 identifier.demo.1. All rights reserved.
+//
+
+import UIKit
+
+class AlbumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BTUIKIT/ViewController/ScreenCollectionView/CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.swift
+//  BTUIKIT
+//
+//  Created by Mac on 1/11/22.
+//  Copyright © 2022 identifier.demo.1. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
